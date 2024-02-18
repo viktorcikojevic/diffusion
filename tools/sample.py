5b4753f00d4292ca889cb93123af88a77ab574d0
viktorcikojevic <cikojevic.viktor@gmail.com>
2024-02-18 19:51:17 +0100

    Update sample.yaml and generate-cifar10-imgs.ipynb

--- tools/sample.py
+++ tools/sample.py
@@ -62,70 +62,75 @@ def main(cfg: DictConfig):
     diffusion_steps_vals = np.arange(num_diffusion_timesteps)[::-1]
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model = model.eval().to(device)
-    img = torch.randn(batch_size, 3, height, width).to(device)
     
-    
-    for step in tqdm(diffusion_steps_vals, total=num_diffusion_timesteps):
-        timestep = torch.Tensor([step]).to(device)
-        z_img = torch.randn(batch_size, 3, height, width).to(device)
-        if step == 0:
-            sigma_t = 0.
-        else:
-            sigma_t = noise_scheduler.sigma_t[step]
-        alpha_t = noise_scheduler.alpha_t[step]
-        alpha_t_bar = noise_scheduler.alpha_t_bar[step]
+    n_iters = cfg_dict["sampler"]["n_iters"]
+    for iter_num in range(n_iters):
+        print(f"Starting iteration {iter_num + 1} of {n_iters}")
         
-        with torch.no_grad():
-            model_out = model.predict(img, timestep).pred
-    
-        img = (1/np.sqrt(alpha_t)) * (img - (1-alpha_t)/np.sqrt(1-alpha_t_bar) * model_out) + sigma_t * z_img
+        img = torch.randn(batch_size, 3, height, width).to(device)
         
-        if save_all:
-            img_np = img.squeeze().cpu().numpy()
-            # convert to pil and save
-            img_np = 255 * (img_np - img_np.min()) / (img_np.max() - img_np.min())
-            img_np = np.transpose(img_np, (1, 2, 0))
-            img_pil = Image.fromarray(np.uint8(img_np))
-            step_process = diffusion_steps - step
-            img_pil.save(save_dir / "imgs" / f"step_{step_process}.png")
-        if save_last:
+        
+        for step in tqdm(diffusion_steps_vals, total=num_diffusion_timesteps):
+            timestep = torch.Tensor([step]).to(device)
+            z_img = torch.randn(batch_size, 3, height, width).to(device)
             if step == 0:
-                img_np = img.cpu().numpy()
+                sigma_t = 0.
+            else:
+                sigma_t = noise_scheduler.sigma_t[step]
+            alpha_t = noise_scheduler.alpha_t[step]
+            alpha_t_bar = noise_scheduler.alpha_t_bar[step]
+            
+            with torch.no_grad():
+                model_out = model.predict(img, timestep).pred
+        
+            img = (1/np.sqrt(alpha_t)) * (img - (1-alpha_t)/np.sqrt(1-alpha_t_bar) * model_out) + sigma_t * z_img
+            
+            if save_all:
+                img_np = img.squeeze().cpu().numpy()
                 # convert to pil and save
-                img_np = 255 * (img_np - np.min(img_np, axis=(1, 2, 3), keepdims=True)) / (np.max(img_np, axis=(1, 2, 3), keepdims=True) - np.min(img_np, axis=(1, 2, 3), keepdims=True))
+                img_np = 255 * (img_np - img_np.min()) / (img_np.max() - img_np.min())
+                img_np = np.transpose(img_np, (1, 2, 0))
+                img_pil = Image.fromarray(np.uint8(img_np))
+                step_process = diffusion_steps - step
+                img_pil.save(save_dir / "imgs" / f"step_{step_process}_{iter_num}.png")
+            if save_last:
+                if step == 0:
+                    img_np = img.cpu().numpy()
+                    # convert to pil and save
+                    img_np = 255 * (img_np - np.min(img_np, axis=(1, 2, 3), keepdims=True)) / (np.max(img_np, axis=(1, 2, 3), keepdims=True) - np.min(img_np, axis=(1, 2, 3), keepdims=True))
+                    
+                    for img_idx in range(img_np.shape[0]):
+                        img_np_t = np.transpose(img_np[img_idx], (1, 2, 0))
+                        img_pil = Image.fromarray(np.uint8(img_np_t))
+                        img_pil.save(save_dir / f"output_{img_idx}_{iter_num}.png")
                 
-                for img_idx in range(img_np.shape[0]):
-                    img_np_t = np.transpose(img_np[img_idx], (1, 2, 0))
-                    img_pil = Image.fromarray(np.uint8(img_np_t))
-                    img_pil.save(save_dir / f"output_{img_idx}.png")
-            
-            
-    if save_all:
-        # convert all the images to a gif
-        # Ensure the images are sorted by their step number
-        img_paths = sorted((save_dir / "imgs").glob("*.png"), key=lambda x: int(x.stem.split('_')[1]))
+                
+        if save_all:
+            # convert all the images to a gif
+            # Ensure the images are sorted by their step number
+            img_paths = sorted((save_dir / "imgs").glob("*.png"), key=lambda x: int(x.stem.split('_')[1]))
 
-        # Load the images
-        imgs = [Image.open(img_path) for img_path in img_paths]
+            # Load the images
+            imgs = [Image.open(img_path) for img_path in img_paths]
 
-        # Save the images as a GIF
-        gif_path = save_dir / "output.gif"
-        imgs[0].save(gif_path, save_all=True, append_images=imgs[1:], optimize=False, duration=100, loop=0)
-        
-        # Convert img_paths to string paths because ImageSequenceClip expects paths
-        img_paths_str = [str(img_path) for img_path in img_paths]
-        
-        # Calculate frame rate to make the video last 15 seconds
-        total_images = len(img_paths)
-        video_duration = 30  # seconds
-        frame_rate = total_images / video_duration
-        
-        # Create a video clip
-        clip = ImageSequenceClip(img_paths_str, fps=frame_rate)
-        
-        # Save the video as an MP4 file
-        mp4_path = str(save_dir / "output.mp4")
-        clip.write_videofile(mp4_path, codec='libx264')
+            # Save the images as a GIF
+            gif_path = save_dir / "output.gif"
+            imgs[0].save(gif_path, save_all=True, append_images=imgs[1:], optimize=False, duration=100, loop=0)
+            
+            # Convert img_paths to string paths because ImageSequenceClip expects paths
+            img_paths_str = [str(img_path) for img_path in img_paths]
+            
+            # Calculate frame rate to make the video last 15 seconds
+            total_images = len(img_paths)
+            video_duration = 30  # seconds
+            frame_rate = total_images / video_duration
+            
+            # Create a video clip
+            clip = ImageSequenceClip(img_paths_str, fps=frame_rate)
+            
+            # Save the video as an MP4 file
+            mp4_path = str(save_dir / "output.mp4")
+            clip.write_videofile(mp4_path, codec='libx264')
 
 
 
